249079365ef3a91f54b10040dd8d0d38ae61c185
Chemicalz82 <chemcope82@yahoo.com>
2018-08-06 18:43:10 -0500

    completed the gif app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Gif-alicious
+
+
+
+## Gif Search
+
+This web program allows a user to add his/her favorite band and generate a new search button.  When the button is clicked, the top 10 trending gifs (using the Giphy API) are displayed.  As many buttons as desired my be made and duplicate or blank submissions are prohibited.  Clicking on an image will animate the gif and clicking it again will return it to still.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@
 
 var bandList = ["Green Day", "Metallica", "Godsmack", ];
 var apiKey = "230WNSN2hzoLsYdb7eEqC7xOyUJ6RStF";
+var currentBand = $("#images");
 
 function renderButton() {
     $("#buttons").empty();
@@ -18,7 +19,7 @@ function renderButton() {
 $("#addBand").on("click", function(event) {
     event.preventDefault();
     var band = $("#bandSearch").val().trim();
-    // band list is rendered how the user types in band name but all entries are converted to lower case for comparison to prevent duplicate entries
+    // band list is rendered based on how the user types in band name but all entries are converted to lower case for comparison to prevent duplicate entries
     var lowerBand = band.toLowerCase();
     var newBandList = [];
     for (var i=0; i<bandList.length; i++) {
@@ -32,5 +33,52 @@ $("#addBand").on("click", function(event) {
     $("#bandSearch").val("");
 });
 
+function showGif(){
+    var band = $(this).attr("data-band");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + band + "&api_key=" + apiKey + "&limit=10&rating=pg";
+    console.log(queryURL);
+    currentBand.empty();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+        var results = response.data;
+        console.log(results);
+
+        for (var i=0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var urlStill = results[i].images.fixed_height_still.url;
+            var urlAnimated = results[i].images.fixed_height.url;
+            var rating = results[i].rating;
+            var gifImage = $("<img>");
+            gifImage.attr("data-still", urlStill);
+            gifImage.attr("data-animate", urlAnimated);
+            gifImage.attr("data-state", "still");
+            gifImage.attr("src", urlStill);
+            gifImage.addClass("gif");
+            gifDiv.append(gifImage);
+            $("#images").append(gifDiv);
+        }
+    })
+}
+
+
+$(document).on("click", ".button", showGif);
+
 
 renderButton();
+
+function animateGif() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+    } else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+};
+
+$(document).on("click", ".gif", animateGif);
